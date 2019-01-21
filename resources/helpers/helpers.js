22131274891336e1a084234ca8d33664dd55d6b5
pwkrz <pg.krzeminski@gmail.com>
2019-01-21 18:20:04 +0100

    endOfImports: added safeguard in case no imports found

--- resources/helpers/helpers.js
+++ resources/helpers/helpers.js
@@ -20,7 +20,7 @@ const nonUpdateableModels = ['authenticationservicemodel', 'baseservicemodel', '
 const REGEXS = {
     fileTemplate: /\*\*(.+?)\*\*/gim,
     todoVerifyImports: /\/\/ TODO verify the following imports\: .+?;/g,
-    endOfImports: /(import(?:.|\s)*?;\s+)((?:\/\/ TODO verify the following imports\: .+;\s+)*)((?:(?:\/{1,2}\*? ?(?:.|\s)+?(?:\/\/|\*\/|;)\s)+|\@NgModule|export class|export const))/m,
+    endOfImports: /((?:import(?:.|\s)*?;\s+)*)((?:\/\/ TODO verify the following imports\: .+;\s+)*)((?:(?:\/{1,2}\*? ?(?:.|\s)+?(?:\/\/|\*\/|;)\s)+|\@NgModule|export class|export const))/m,
     modelPropDeclarations: /(class \w+ (?:(?:implements|extends) [\w\<\>\[\]]+ )?{\s+)((?:[_wu]?id\??\: string\;){0,4})((?:(?:\r|\n|\r\n)?\s*\w+\??(?: ?[\:\=] ?.*\;|\;))*)/m,
     serverModelMapMethods: /(static (map(?:Reverse)?) ?\((\w+?)\: (?:Server)?\w+\)\: (?:Server)?\w+? \{\s*?const (\w+?) ?\= ?.*?;)((?:(?:\r|\n|\r\n)?\s*\4\.[_w]?id ?\= ?.*\3\..+;){0,3})((?:(?:\r|\n|\r\n)?\s*\4\.\w+ ?\= ?.*\3\..+)*)/gm,
     modelPropLine: /(\w+?)(\??)((?: ?\: ?.*)|$|(?: ?\= ?(.*)))/,
